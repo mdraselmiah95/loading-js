dd6b0620f1057ff1abbb74ab25551700da4c0570
rasel <russelroy15@gmail.com>
2021-09-03 13:23:00 +0600

    new meal added

--- app.js
+++ app.js
@@ -9,17 +9,32 @@ const displaySingleUser = (user) => {
   //   console.log(user);
 };
 
-const searchMeal = () => {
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=fish")
+//Random users
+
+//Display meals
+const displayMeal = document.getElementById("display-meals");
+
+const searchMeal = (searchMeals) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeals}`;
+  fetch(url)
     .then((res) => res.json())
-    .then((data) => displayMeal(data.meals));
+    .then((data) => displayMeals(data.meals));
 };
 
-searchMeal();
+searchMeal("fish");
 
-const displayMeal = (meals) => {
+const displayMeals = (meals) => {
   //   console.log(meals);
   meals.forEach((meal) => {
     console.log(meal);
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <img class="img-fluid border border-1 rounded" src="${meal.strMealThumb}" alt="meal image">
+        <h3 class="text-warning">${meal.strMeal}</h3>
+        <h4>${meal.strTags}</h4>
+        <button class="btn btn-outline-warning mb-3">CLICK ME</button>
+      `;
+    displayMeal.appendChild(div);
   });
 };
