9918ac6fae777cd63c3d20c4744758ab62f73c88
rasel <russelroy15@gmail.com>
2021-09-03 12:59:47 +0600

    testing API

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(99);
+const loadSingleUser = () => {
+  fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => displaySingleUser(data.results[0]));
+};
+loadSingleUser();
+
+const displaySingleUser = (user) => {
+  //   console.log(user);
+};
+
+const searchMeal = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
+searchMeal();
